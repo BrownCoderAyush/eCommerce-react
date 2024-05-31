d10c788313fef020a7e6f1877eebc611f5c202d2
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-06-01 00:16:27 +0530

    added navbar component using reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
-    <>
-    </>
+  <Header color='light' light={true} expand='md' container='md' />
   )
 }
 
