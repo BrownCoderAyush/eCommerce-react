fc1f9016aad861f690fc559f4fb10b3339eb429a
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-06-04 19:16:19 +0530

    added Product Details Page with css

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,5 +1,32 @@
-function ProductList(){
-    return <>ProductList</>
+import ProductBox from '../../components/ProductBox/ProductBox';
+import FilterProducts from '../../components/FilterProducts/FilterProducts';
+
+import './ProductList.css';
+
+import ProductImage from '../../assets/product.jpg';
+
+function ProductList() {
+    return (
+        <div className="container">
+            <div className="row">
+                <h2 className='product-list-title text-center'>All Products</h2>
+                
+                {/* list of products */}
+                <div className="product-list-wrapper d-flex flex-row">
+
+                    <FilterProducts/>
+                    <div className="product-list-box" id="productList">
+                        <ProductBox 
+                            productImage={ProductImage}
+                            name={"Dummy"}
+                            price={100}
+                        />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default ProductList;
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,15 +2,18 @@ import { Routes , Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Error from "../pages/Error/Error";
 import ProductList from "../pages/ProductList/ProductList";
+import ProductDetails from "../components/ProductDetails/ProductDetails";
+
 function MainRoutes(){
 
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/products" element={<ProductList/>}/>
+            <Route path="/products/:id" element={<ProductDetails/>}/>
             <Route path="*" element={<Error/>}/>
         </Routes>
-    )
+    )   
 
 }
 
